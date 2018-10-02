d2920ba3955e1e4611ce7fe889d102bd634acbeb
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-10-02 04:50:45 -0400

    so now i almost have a register and login. i gonna have a database with stored users on it soon

--- app/src/main/java/com/example/stephanie/wehere/MainActivity.java
+++ app/src/main/java/com/example/stephanie/wehere/MainActivity.java
@@ -1,13 +1,164 @@
 package com.example.stephanie.wehere;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.example.stephanie.wehere.Model.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSignIn, btnRegister;
+    RelativeLayout rootLayout;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+
+
+    @Override
+    protected void attachBaseContext(Context newBase){
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                                            .setDefaultFontPath("fonts/Longhaul.ttf")
+                                            .setFontAttrId(R.attr.fontPath)
+                                            .build());
         setContentView(R.layout.activity_main);
+
+        //Firebase
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+        //Event CLicks
+        btnRegister = findViewById(R.id.btnRegister);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        rootLayout = findViewById(R.id.rootLayout);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              showRegisterDialog();
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLoginDialog();
+            }
+        });
+    }
+
+    private void showLoginDialog(){
+        
+    }
+
+    private void showRegisterDialog(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("REGISTER ");
+        dialog.setMessage("Please use email to register");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View register_layout = inflater.inflate(R.layout.layout_register, null);
+
+        final MaterialEditText edtEmail = register_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPassword = register_layout.findViewById(R.id.edtPassword);
+        final MaterialEditText edtName = register_layout.findViewById(R.id.edtName);
+        final MaterialEditText edtPhone = register_layout.findViewById(R.id.edtPhone);
+
+        dialog.setView(register_layout);
+
+        dialog.setPositiveButton("REGISTER", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter email address", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtPhone.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter phone number", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtPassword.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if ((edtPassword.getText().toString().length() < 6)) {
+                    Snackbar.make(rootLayout, "Password too short!", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty((edtName.getText().toString()))) {
+                    Snackbar.make(rootLayout, "Please enter name", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Register new user
+                auth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>(){
+                            @Override
+                            public void onSuccess(AuthResult authResult){
+                                //save user to database
+                                User user = new User();
+                                user.setEmail(edtEmail.getText().toString());
+                                user.setName(edtName.getText().toString());
+                                user.setPassword(edtPassword.getText().toString());
+                                user.setPhone(edtPhone.getText().toString());
+
+                                //sets email as key
+                                users.child(user.getEmail())
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Snackbar.make(rootLayout, "Register successful!", Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Snackbar.make(rootLayout, "Failed "+ e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(rootLayout, "Failed "+ e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        dialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
